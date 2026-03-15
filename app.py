1106543b0ced40fd4c78ffe6bf4d24ad389e035b
MOHIT KUSHWAHA <105541079+Mohit-2022@users.noreply.github.com>
2026-03-15 23:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
