bb5fa35d3177f25958469a79839ca14d27b18b56
MOHIT KUSHWAHA <105541079+Mohit-2022@users.noreply.github.com>
2026-03-15 22:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def load_chain():
 
     # Load Gemini
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-2.0-flash",
         google_api_key=st.secrets["GOOGLE_API_KEY"],
         temperature=0,
         convert_system_message_to_human=True
